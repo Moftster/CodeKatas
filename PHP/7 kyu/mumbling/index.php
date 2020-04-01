0722da8c936fcb7eed54391e9cc86305e9c04e9f
David Moftakhar <david@sellmysnaps.com>
2020-04-01 19:36:47 +0100

    Completed mumbling kata.

--- PHP/7 kyu/mumbling/index.php
+++ PHP/7 kyu/mumbling/index.php
@@ -1,7 +1,30 @@
 <?php
 
 function accum($s) {
+    // Split into array
+    $stringToArray = str_split($s);
+    // print_r($stringToArray);
+
+    // Use index of array plus one to generate the number of letters
+    $stringWithMultipliedNumbers = "";
     
+    $multiplier = 1;
+
+    foreach ($stringToArray as $individualLetter) {
+        $letters = str_repeat($individualLetter, $multiplier);
+        $FirstLetterUC = ucfirst(strtolower($letters));
+        $toBeAppended = "-" . $FirstLetterUC;
+        $stringWithMultipliedNumbers .= $toBeAppended;
+        $multiplier += 1;
+    }
+
+    $finalStringWithOpeningDashRemoved = ltrim($stringWithMultipliedNumbers, '-');
+
+    echo $finalStringWithOpeningDashRemoved;
+
+    return $finalStringWithOpeningDashRemoved;
+
+    // Convert back to string with dash in between
 }
 
 
