a6f4cf77142e8e6e2e8bbbb0e342e0edc922c5c2
David Moftakhar <david@sellmysnaps.com>
2020-04-02 11:19:08 +0100

    Sort descending order kata completed.

--- PHP/7 kyu/descending-order/index.php
+++ PHP/7 kyu/descending-order/index.php
@@ -1,7 +1,19 @@
 <?php
 
 function descendingOrder(int $n): int {
-  // Replace with your code
+
+    $convertIntegerToArray  = array_map('intval', str_split($n));
+
+    rsort($convertIntegerToArray);
+
+    // print_r($convertIntegerToArray);
+
+    $toInteger = intval(implode($convertIntegerToArray));
+
+    print_r($toInteger);
+
+    return $toInteger;
 }
 
 descendingOrder(1021);
+descendingOrder(9);
