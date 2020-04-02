3a3ba5a77c62d9c2ce25c74176e04bc8ae887b86
David Moftakhar <david@sellmysnaps.com>
2020-04-02 12:10:16 +0100

    Completed kata.

--- PHP/7 kyu/jaden-casings/index.php
+++ PHP/7 kyu/jaden-casings/index.php
@@ -4,7 +4,22 @@ function toJadenCase($string)
 {
     //Convert sentence into array
 
+    $sentenceToArrayOfWords = explode(" ", $string);
+    // print_r($sentenceToArrayOfWords);
+
     //For each word capitalise first letter
+
+    $theString = "";
+
+    foreach($sentenceToArrayOfWords as $word) {
+        $capitalisedWord = ucfirst($word);
+        $toBeAppended = $capitalisedWord . " ";
+        $theString .= $toBeAppended;
+    }
+
+    echo rtrim($theString);
+    
+    return rtrim($theString);
 
     //Convert array of capitalised words back into string
 }
