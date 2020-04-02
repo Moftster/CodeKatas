880ba158b653806cdbbd22590c02f65533d04c46
David Moftakhar <david@sellmysnaps.com>
2020-04-02 12:11:01 +0100

    Alternative solution.

--- PHP/7 kyu/jaden-casings/index.php
+++ PHP/7 kyu/jaden-casings/index.php
@@ -2,26 +2,9 @@
 
 function toJadenCase($string) 
 {
-    //Convert sentence into array
 
-    $sentenceToArrayOfWords = explode(" ", $string);
-    // print_r($sentenceToArrayOfWords);
+    echo ucwords($string);
 
-    //For each word capitalise first letter
-
-    $theString = "";
-
-    foreach($sentenceToArrayOfWords as $word) {
-        $capitalisedWord = ucfirst($word);
-        $toBeAppended = $capitalisedWord . " ";
-        $theString .= $toBeAppended;
-    }
-
-    echo rtrim($theString);
-    
-    return rtrim($theString);
-
-    //Convert array of capitalised words back into string
 }
 
 toJadenCase("How can mirrors be real if our eyes aren't real");
