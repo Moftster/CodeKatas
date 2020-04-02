1b782c6dba63ad4ffa2f1361b4e6e4762b5b4791
David Moftakhar <david@sellmysnaps.com>
2020-04-02 11:24:54 +0100

    Approach to completing kata.

--- PHP/7 kyu/jaden-casings/index.php
+++ PHP/7 kyu/jaden-casings/index.php
@@ -2,7 +2,11 @@
 
 function toJadenCase($string) 
 {
-   //...
+    //Convert sentence into array
+
+    //For each word capitalise first letter
+
+    //Convert array of capitalised words back into string
 }
 
 toJadenCase("How can mirrors be real if our eyes aren't real");
