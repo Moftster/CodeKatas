d98c30930bd654ec10b11a022211b19862279c0c
David Moftakhar <david@sellmysnaps.com>
2020-04-02 10:12:24 +0100

    FizzBuzz exercise completed.

--- PHP/8 kyu/FizzBuzz/index.php
+++ PHP/8 kyu/FizzBuzz/index.php
@@ -1,30 +1,19 @@
 <?php
 
-for($i=1; $i<=100; $i++) {
+function FizzBuzz() {
+    
+    for($i = 1; $i <= 100; $i++) {
 
-  if($i % 3 == 0 && $i % 5 == 0) {
-
-    echo "FizzBuzz <br>";
-
-  } elseif ($i % 3 == 0) {
-
-    echo "Fizz<br>";
-
-  } elseif ($i % 5 == 0) {
-
-    echo "Buzz<br>";
-  } else{
-
-    echo "This is number " . $i . "<br>";
+        if($i % 3 == 0 && $i % 5 == 0) {
+            echo "FizzBuzz <br>";
+        } elseif($i % 3 == 0) {
+            echo "Fizz <br>";
+        } elseif($i % 5 == 0) {
+            echo "Buzz <br>";
+        } else {
+        echo $i . "<br>";
+        }
+    }
 }
 
-}
-
-
- ?>
-
-<script>
-
-console.log("Hello console");
-
-</script>
+FizzBuzz();
