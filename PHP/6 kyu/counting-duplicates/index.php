39ffb5208c1f6199eeb5039f6bc18f5f2c5bc4ad
David Moftakhar <david@sellmysnaps.com>
2020-04-05 16:46:03 +0100

    Kata completed.

--- PHP/6 kyu/counting-duplicates/index.php
+++ PHP/6 kyu/counting-duplicates/index.php
@@ -1,9 +1,47 @@
 <?php 
 
 function duplicateCount($text) {
-  // ...
+
+if(strlen($text) > 0) {
+
+$stringToLowerCase = strtolower($text);
+$stringToArray = str_split($stringToLowerCase);
+$repeatedCharacters = "";
+
+    foreach($stringToArray as $individualCharacter) {
+
+        $count = substr_count($stringToLowerCase, $individualCharacter);
+
+        // print_r($count);
+
+        if($count > 1) {
+            $repeatedCharacters .= $individualCharacter;
+            // echo $repeatedCharacters;
+        }
+    }
+
+    // echo $repeatedCharacters;
+
+    if($repeatedCharacters){
+
+    echo count(array_unique(str_split($repeatedCharacters)));
+    return count(array_unique(str_split($repeatedCharacters)));
+    } else {
+        echo "No matches";
+        return 0;
+    }
+
+} else {
+    echo "Empty String";
+    return 0;
+}
+
 }
 
 
+// duplicateCount("aabBcde0412551");
+// duplicateCount("Indivisibilities");
+duplicateCount("");
+duplicateCount("abcde");
 duplicateCount("aabBcde");
-duplicateCount("Indivisibilities");
+// duplicateCount("Indivisibility");
