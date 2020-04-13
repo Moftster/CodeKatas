6f3bdccaeaa023148e876b8e51ff08d8eb821fe7
David Moftakhar <david@sellmysnaps.com>
2020-04-13 15:49:10 +0100

    How answer will be architected.

--- PHP/5-kyu/direction-reduction/index.php
+++ PHP/5-kyu/direction-reduction/index.php
@@ -3,6 +3,13 @@
 function dirReduc($arr){
 
     // Convert to same case
+
+    // Count values in array
+
+    // North vs South - if one higher than the other it goes to array
+
+    // East vs West - if one higher than the other it goes to array
+    
     foreach($arr as $a) {
         echo $a . "<br>";
     }
