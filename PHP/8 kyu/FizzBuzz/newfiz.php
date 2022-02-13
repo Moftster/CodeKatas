51d299ec30398cd3e6377c98dd32ca04f49afa7c
dmoftakhar <david@chillaweb.com>
2022-02-13 16:54:03 +0000

    Roman practice and FizzBuzz.

---
+++ PHP/8 kyu/FizzBuzz/newfiz.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+function fizzBuzzer () {
+    for ($i=1; $i<=100; $i++) {
+        if ((($i % 5) == 0) && (($i % 3) == 0)) {
+            echo $i . " FizzBuzz \n";
+        } else if (($i % 3) == 0) {
+            echo $i . " Fizz \n";
+        } else if (($i % 5) == 0) {
+            echo $i . " Buzz \n";
+        }
+    }
+};
+
+fizzBuzzer();
